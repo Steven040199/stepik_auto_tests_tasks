9363cdd326cbf88c22dcd393b75fb93a25826e2a
Steven040199 <133018098+Steven040199@users.noreply.github.com>
2023-05-10 17:27:05 +0300

    Add files via upload

---
+++ link_scrp.py
@@ -0,0 +1,23 @@
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+import time
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+import json
+
+link = "https://thehackernews.com/"
+
+try:
+    browser = webdriver.Chrome()
+    browser.get(link)
+    lnks = browser.find_elements(By.TAG_NAME, "a")
+    for lnk in lnks:
+        if "Hack" in lnk.text:
+            answer = "Link text: " + lnk.get_attribute("href")
+            #print(json.dumps(answer, indent=4))
+            print(answer)
+finally:
+    time.sleep(5)
+    browser.quit()
+    exit(0)
+
